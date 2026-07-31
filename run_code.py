b3a0d700ccfb2ff07c2675819b9ca374a1e32281
scott <hericscott033@gmail.com>
2026-07-31 08:44:24 +0100

    trying out some code

---
+++ run_code.py
@@ -0,0 +1,14 @@
+#What happens when you run the following code? Why?
+#solution
+
+NAME = 'Victor'
+print('Good Morning, ' + NAME)
+print('Good Afternoon, ' + NAME)
+print('Good Evening, ' + NAME)
+
+NAME = 'Nina'
+print('Good Morning, ' + NAME)
+print('Good Afternoon, ' + NAME)
+print('Good Evening, ' + NAME)
+
+
