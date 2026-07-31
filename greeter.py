42ef82e89f7903f5ec949a2bfb29895e329d60b3
scott <hericscott033@gmail.com>
2026-07-31 08:25:56 +0100

    A written program

---
+++ greeter.py
@@ -0,0 +1,9 @@
+#Write a program named greeter.py that greets 'Victor' three times. Your program should use a variable and not hard code the string value 'Victor' in each greeting. Here's an example run of the program:
+
+#solution
+
+name = "victor"
+
+print(f"Good morning, {name}.")
+print(f"Good Afternoon, {name}.")
+print(f"Good Evening, {name}.")
