1ae862c25de758632ca1d3b3c6b65957f88c3578
scott <hericscott033@gmail.com>
2026-07-31 09:11:06 +0100

    balance

---
+++ balance.py
@@ -0,0 +1,12 @@
+#Repeat the previous question but, this time, use augmented assignment to compute the final result, one year at a time.
+#solution
+
+balance = 1000.00
+
+balance *= 1.05
+balance *= 1.05
+balance *= 1.05
+balance *= 1.05
+balance *= 1.05
+
+print(balance)
